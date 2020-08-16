2ee30f05c5cbbb94c5ee2fc759240fcf9a84d9ad
fixset <clintonksang@gmail.com>
2020-08-16 16:46:53 +0300

    SearchScreen

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -14,7 +14,6 @@ import 'package:google_sign_in/google_sign_in.dart';
 
 import '../main.dart';
 
-
 class HomeScreen extends StatefulWidget {
   final String currentUserId;
   HomeScreen({Key key, @required this.currentUserId}) : super(key: key);
@@ -23,45 +22,62 @@ class HomeScreen extends StatefulWidget {
 }
 
 class HomeScreenState extends State<HomeScreen> {
+
 //HomePageFunctions
-homePageHeader(){
-return AppBar(
-  automaticallyImplyLeading: false, //removes back button
-  actions: <Widget>[
-    IconButton(icon: Icon(Icons.settings_applications,size:30,color:Colors.white), onPressed:(){ Navigator.push(context, MaterialPageRoute(builder: (context)=>Settings()));}
-    )]
-);
-}
+TextEditingController searchEditingController= TextEditingController();
+  homePageHeader() {
+    return AppBar(
+      automaticallyImplyLeading: false, //removes back button
+
+      actions: <Widget>[
+        IconButton(
+            icon: Icon(Icons.settings_applications,
+                size: 30, color: Colors.white),
+            onPressed: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => Settings()));
+            })
+      ],
+
+      backgroundColor: Colors.lightBlueAccent,
+
+      title: Container(
+        margin: EdgeInsets.only(bottom: 4.0),
+        child: TextFormField(
+          style: TextStyle(fontSize: 18, color: Colors.white),
+          controller: searchEditingController,
+          
+        ),
+      ),
+    );
+  }
 
 //signin functions
-  final GoogleSignIn googleSignIn= GoogleSignIn();
-Future<Null> logoutUser()async{
-  await FirebaseAuth.instance.signOut();
-  await googleSignIn.disconnect();
-  await googleSignIn.signOut();
- Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context)=>MyApp()
-  ),  (Route<dynamic> route)=>false);
-}
-
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  Future<Null> logoutUser() async {
+    await FirebaseAuth.instance.signOut();
+    await googleSignIn.disconnect();
+    await googleSignIn.signOut();
+    Navigator.of(context).pushAndRemoveUntil(
+        MaterialPageRoute(builder: (context) => MyApp()),
+        (Route<dynamic> route) => false);
+  }
 
   @override
   Widget build(BuildContext context) {
-    
     // RaisedButton.icon(
-    
+
     return Scaffold(
       appBar: homePageHeader(),
-      body: RaisedButton.icon(  onPressed: (logoutUser),
-       icon: Icon(Icons.power_settings_new),
-        label: Text('Log Out')),
+      body: RaisedButton.icon(
+          onPressed: (logoutUser),
+          icon: Icon(Icons.power_settings_new),
+          label: Text('Log Out')),
     );
   }
 }
 
-class UserResult extends StatelessWidget
-{
+class UserResult extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-
-  }
+  Widget build(BuildContext context) {}
 }
